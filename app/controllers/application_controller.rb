2b84a137e152923090ff32688895986952f5faa3
ttkirkland <tkirk@brandeis.edu>
2011-07-19 16:22:30 -0400

    bug fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   USER_NAME, PASSWORD = "dhh", "secret"
   before_filter :authenticate
+  def logged_in?
+    true
+  end
   private
     def authenticate
       authenticate_or_request_with_http_basic do |user_name, password|
