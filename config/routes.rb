0cb9bbfda0e72da90eec15de4548e79bc71747c3
ttkirkland <tkirk@brandeis.edu>
2011-07-19 16:26:46 -0400

    pretty default page

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Happytrials::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root :to => redirect("/admin_data")
+    root :to => redirect("/admin_data/quick_search/user")
    
   # The priority is based upon order of creation:
   # first created -> highest priority.
